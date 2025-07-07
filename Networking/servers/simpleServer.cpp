433ef31a79cc0d5a09aec60d68d9b9908037f93b
HariomRajChauhan <chauhanhariomraj7@gmail.com>
2025-07-07 07:12:10 +0545

    simpleserver definition added

--- Networking/servers/simpleServer.cpp
+++ Networking/servers/simpleServer.cpp
@@ -1,1 +1,11 @@
 #include "simpleServer.hpp"
+
+HRS::simpleServer::simpleServer(int domain, int service, int protocol, int port, u_long interfac, int bklg)
+{
+    socket = new listeningSocket(domain, service, protocol, port, interfac, bklg);
+    // delete socket;
+}
+
+HRS::listeningSocket* HRS::simpleServer::get_socket(){
+    return socket;
+}
--- Networking/servers/simpleServer.hpp
+++ Networking/servers/simpleServer.hpp
@@ -11,8 +11,14 @@ namespace HRS
     {
         private:
             listeningSocket * socket;
+            virtual void acceptor() = 0;
+            virtual void handaler() = 0;
+            virtual void responder() = 0;
+
         public:
             simpleServer(int domain, int service, int protocol, int port, u_long interfac, int bklg);
+            virtual void lunch() = 0;
+            listeningSocket* get_socket();
     };
 }
 
